c51ac78100cced77d56478e2b889dd22f968ec8d
Vukasin <vule993@outlook.com>
2020-03-22 15:14:02 +0100

    navigation
    slide navigation

--- Reservations-app/src/app/app.module.ts
+++ Reservations-app/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { HeaderComponent } from './header/header.component';
-import { NavbarComponent } from './header/navbar/navbar.component';
-import { SearchComponent } from './header/search/search.component';
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+
+import { NavigationComponent } from "./navigation/navigation.component";
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent, HeaderComponent, NavbarComponent, SearchComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavigationComponent
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Reservations-app/src/app/navigation/navigation.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-navigation",
+  templateUrl: "./navigation.component.html",
+  styleUrls: ["./navigation.component.css"]
+})
+export class NavigationComponent implements OnInit {
+  open = false;
+  constructor() {}
+  onClick(event) {
+    var nav = document.getElementsByTagName("nav")[0];
+    var navigation = document.getElementById("navigation");
+
+    if (!this.open) {
+      nav.classList.add("transform-active");
+
+      let timer = setTimeout(() => {
+        navigation.classList.remove("display-none");
+      }, 500);
+
+      this.open = true;
+    } else {
+      nav.classList.remove("transform-active");
+
+      let timer = setTimeout(() => {
+        navigation.classList.add("display-none");
+      }, 10);
+
+      this.open = false;
+    }
+  }
+  ngOnInit(): void {}
+}
