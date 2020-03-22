201fce4f4e72854bfc939fdae7f1be97ec90a6ab
Aleksandar <sale.novakovic403@gmail.com>
2020-03-22 11:30:37 +0100

    navbar with login and reg

--- Reservations-app/src/app/app.module.ts
+++ Reservations-app/src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
+import { SearchComponent } from './header/search/search.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  declarations: [AppComponent, LoginComponent, RegisterComponent, HeaderComponent, NavbarComponent, SearchComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
