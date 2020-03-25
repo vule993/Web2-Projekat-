f16da9ddb82c015656c100d32dc08898c5c71341
Vukasin <vule993@outlook.com>
2020-03-25 09:18:32 +0100

    profile, skaliranje navigacije i profila
    izdeljene sekcije profila svi elementi podeseni da se skaliraju kako bi izgled bio gotovo identican na svim ekranima

--- Reservations-app/src/app/content/profile/profile.component.ts
+++ Reservations-app/src/app/content/profile/profile.component.ts
@@ -9,4 +9,27 @@ export class ProfileComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onProfilePictureHover() {
+    var element = document.getElementById("photo-icon");
+    let i = 0;
+    let animation = setInterval(() => {
+      element.style.opacity = i.toString();
+      i += 0.1;
+      if (i > 0.6) {
+        clearInterval(animation);
+      }
+    }, 50);
+  }
+  onProfilePictureLeave() {
+    var element = document.getElementById("photo-icon");
+    let i = 0.6;
+    let animation = setInterval(() => {
+      element.style.opacity = i.toString();
+      i -= 0.1;
+      if (i < 0) {
+        clearInterval(animation);
+      }
+    }, 50);
+  }
 }
