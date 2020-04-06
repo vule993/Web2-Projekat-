7fea68e357a747dc0c3ae1dfad6cddaafd9807e3
Vukasin <vule993@outlook.com>
2020-04-06 16:18:16 +0200

    Revert "starting design of landing page"
    This reverts commit 0f4a43ba60862953a9b59351fa57c04701587da1.

--- Reservations-app/src/app/app.module.ts
+++ Reservations-app/src/app/app.module.ts
@@ -17,9 +17,8 @@ import { ArchiveDataListComponent } from "./content/profile/archive-data-list/ar
 import { ReservationsDataListComponent } from "./content/profile/reservations-data-list/reservations-data-list.component";
 import { EditProfileComponent } from "./content/profile/edit-profile/edit-profile.component";
 import { OfferItemComponent } from "./content/offers/offers-list/offer-item/offer-item.component";
-import { OfferDetailComponent } from "./content/offers/offer-detail/offer-detail.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HomeComponent } from "./content/home/home.component";
+import { OfferDetailComponent } from './content/offers/offer-detail/offer-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -36,8 +35,7 @@ import { HomeComponent } from "./content/home/home.component";
     ArchiveDataListComponent,
     ReservationsDataListComponent,
     EditProfileComponent,
-    OfferDetailComponent,
-    HomeComponent
+    OfferDetailComponent
   ],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
