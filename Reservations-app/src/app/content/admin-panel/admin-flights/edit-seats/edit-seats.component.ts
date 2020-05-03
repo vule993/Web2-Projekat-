94a221571d3a2df28c6cd3feebc3307bf9ab2dce
Vukasin <vule993@outlook.com>
2020-05-03 10:40:53 +0200

    seat configuration

--- Reservations-app/src/app/content/admin-panel/admin-flights/edit-seats/edit-seats.component.ts
+++ Reservations-app/src/app/content/admin-panel/admin-flights/edit-seats/edit-seats.component.ts
@@ -20,17 +20,22 @@ export class EditSeatsComponent implements OnInit {
   row_no = 0;
   seats_per_row = 0;
 
-  fieldsNo = 7;
+  animation_length = 2000;
+  d = 17;
+  measure_unit = "vw";
+  current_position = 0;
 
-  segmentsHeight = 0;
-  segmentsCount = 0;
-  segmentsWidth = [0, 0, 0, 0];
-  displayForm = [true, false, false, false, false, false, false];
+  idName = "formId";
+  fieldsNo = 6;
+
+  displayForm = [false, false, false, false, false, false];
+  formValues = [0, 0, 0, 0, 0, 0];
   rows = [];
 
   constructor() {}
+
   configName(value, next) {
-    if (value !== "") {
+    if (value.data !== null) {
       this.displayForm[next] = true;
     } else {
       this.rows = [];
@@ -39,86 +44,83 @@ export class EditSeatsComponent implements OnInit {
       }
     }
   }
+  /*
+  kada se promeni segments number potrebno je ucitati 
+  prvi sledeci select this.formValues[1]
+  */
   displayNext(element, next) {
-    let value = element.target.value;
+    let value = parseInt(element.target.value);
     this.rows = [];
-    //ovde obradjujemo select-e
-    if (isNaN(value)) {
-      if (next === 2) {
-        this.segmentsHeight = 0;
-      }
-      if (next === 3) {
-        this.segmentsCount = 0;
-      }
-      if (next === 4) {
-        this.segmentsWidth[0] = 0;
-        this.seats_per_row = this.segmentsWidth.reduce((a, b) => a + b, 0);
-      }
-      if (next === 5) {
-        this.segmentsWidth[1] = 0;
-        this.seats_per_row = this.segmentsWidth.reduce((a, b) => a + b, 0);
-      }
-      if (next === 6) {
-        this.segmentsWidth[2] = 0;
-        this.seats_per_row = this.segmentsWidth.reduce((a, b) => a + b, 0);
-      }
-      if (next === 7) {
-        this.segmentsWidth[3] = 0;
-        this.seats_per_row = this.segmentsWidth.reduce((a, b) => a + b, 0);
-      }
-      for (let i = next; i <= this.fieldsNo; i++) {
-        this.displayForm[i] = false;
-      }
-    } else {
-      //setujem vrednosti
-      if (next === 2) {
-        this.segmentsHeight = parseInt(value);
-      }
-      if (next === 3) {
-        this.segmentsCount = parseInt(value);
-        for (let i = 3; i < 7; i++) {
-          this.displayForm[i] = false;
-          this.segmentsWidth[i - 3] = 0;
-          //element.target.value = "Number of segments";
+
+    if (!isNaN(value)) {
+      //setujem novu vrednost
+      this.formValues[next - 1] = value;
+
+      //fix za povecanje broja segmenata
+      if (next == 2) {
+        for (let i = 2; i < 2 + this.formValues[1]; i++) {
+          if (this.displayForm[i] === false) {
+            this.displayForm[i] = true;
+            break;
+          }
         }
-        this.seats_per_row = this.segmentsWidth.reduce((a, b) => a + b, 0);
-      }
-      if (next === 4) {
-        this.segmentsWidth[0] = parseInt(value);
-        this.seats_per_row = this.segmentsWidth.reduce((a, b) => a + b, 0);
-      }
-      if (next === 5) {
-        this.segmentsWidth[1] = parseInt(value);
-        this.seats_per_row = this.segmentsWidth.reduce((a, b) => a + b, 0);
-      }
-      if (next === 6) {
-        this.segmentsWidth[2] = parseInt(value);
-        this.seats_per_row = this.segmentsWidth.reduce((a, b) => a + b, 0);
-      }
-      if (next === 7) {
-        this.segmentsWidth[3] = parseInt(value);
-        this.seats_per_row = this.segmentsWidth.reduce((a, b) => a + b, 0);
+
+        for (let i = 2 + this.formValues[1]; i < this.fieldsNo; i++) {
+          if (this.displayForm[i] === true) {
+            this.displayForm[i] = false;
+            this.formValues[i] = 0;
+          }
+        }
       }
 
-      //prikazujem sledeci
-      if (next < 3 + this.segmentsCount) {
+      //prikazem naredni select
+      if (next < 2 + this.formValues[1]) {
         this.displayForm[next] = true;
       }
+
+      //
+    } else {
+      this.hideSelects(next, this.fieldsNo);
+      this.unsetFormValues(next - 1, this.fieldsNo);
     }
 
-    //iscrtavam raspored sedista
-    for (let i = 0; i < this.segmentsHeight; i++) {
+    //iscrtam prikaz sedista
+    this.displayChanges();
+  }
+
+  displayChanges() {
+    for (let i = 0; i < this.formValues[0]; i++) {
       this.rows[i] = new Row();
-      for (let j = 0; j < this.seats_per_row; j++) {
-        this.rows[i].seats[j] = new Seat(i * this.seats_per_row + j + 1);
+      for (let j = 0; j < this.getRowWidth(); j++) {
+        this.rows[i].seats[j] = new Seat(i * this.getRowWidth() + j + 1);
       }
     }
+  }
+  hideSelects(from, to) {
+    for (let i = from; i < to; i++) {
+      this.displayForm[i] = false;
+      //$(this.idName + i)[0].value = 0;
+    }
+  }
+  unsetFormValues(from, to) {
+    for (let i = from; i < to; i++) {
+      this.formValues[i] = 0;
+      $("#" + this.idName + i).val("*");
+    }
+  }
+  getRowWidth() {
+    return (
+      this.formValues[2] +
+      this.formValues[3] +
+      this.formValues[4] +
+      this.formValues[5]
+    );
   }
 
   onSeatClick(event) {
     let seat_number = parseInt(event.toElement.innerText) - 1;
-    let row_number = Math.floor(seat_number / this.seats_per_row);
-    let col_number = seat_number % this.seats_per_row;
+    let row_number = Math.floor(seat_number / this.getRowWidth());
+    let col_number = seat_number % this.getRowWidth();
 
     if (event.toElement.classList.contains("taken-seat")) {
       this.rows[row_number].seats[col_number].taken = false;
@@ -126,6 +128,38 @@ export class EditSeatsComponent implements OnInit {
       this.rows[row_number].seats[col_number].taken = true;
     }
   }
+  prev() {
+    var pozicija = $(".klizac").get(0).style.left;
+    //var trenutna = pozicija.split(".")[0];
+    if (this.current_position == 0) {
+    } else {
+      if (!$(".klizac").is(":animated")) {
+        this.current_position++;
 
+        $(".klizac").animate(
+          { left: this.current_position * this.d + this.measure_unit },
+          this.animation_length,
+          "linear"
+        );
+      }
+    }
+  }
+  next() {
+    var pozicija = $(".klizac").get(0).style.left;
+    //2 je broj elemenata u slajderu van vidnog polja
+    //(n - 4 - jer se 4 vide) ?mozda 2 - 1
+    if (Math.abs(this.current_position) >= 2) {
+    } else {
+      if (!$(".klizac").is(":animated")) {
+        this.current_position--;
+
+        $(".klizac").animate(
+          { left: this.current_position * this.d + this.measure_unit },
+          this.animation_length,
+          "linear"
+        );
+      }
+    }
+  }
   ngOnInit(): void {}
 }
